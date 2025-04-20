bb6efe81595b570f3fcfb5b7820a66bd2dc914a7
yuumuu <haidar.alhazen93@gmail.com>
2025-04-20 16:26:46 +0700

    Fix the Code on file array.cpp

--- array.cpp
+++ array.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int nilai[] = {2,5,9,10,34};
-int skor[5] = {8,12,18,5};
-string nama[3];
+int nilai[] = {1, 2, 4, 7};
+int nilaiDua[3] = {1, 2, 3};
+string nama[4];
 
 int main(){
-    cout << "skor ke 3 = " << skor[2] << endl;
-    skor[2] = 25;
-    cout << "skor ke 3 = " << skor[2] << endl;
+    cout << "Nilai array ke 3 = " << nilaiDua[2] << endl;
+    nilaiDua[2] = 100;
+    cout << "Nilai array ke 3 = " << nilaiDua[2] << endl;
 
-    for(int i = 0; i < 3; i++) {
-        cout << "Nama Mahasiswa ke-" << i+1 << " = ";
+    for(int i = 0; i < 4; i++) {
+        cout << "Masukkan nama ke-" << i + 1 << " = ";
         cin >> nama[i];
     }
 
-    cout<<endl;
+    cout << endl;
+    cout << "Menampilkan Array Nama" << endl;
+    cout << "======================" << endl;
 
-    for(int i = 0; i < 3; i++){
-        cout << "Data Mahasiswa ke-" << i+1 << " = " << nama[i]
+    for(int i = 0; i < 4; i++) {
+        cout << "Nama ke-" << i + 1 << " = " << nama[i]
         << endl;
     }
 }
