d05a526443496c37f773126f1167f72a9a4dc0ce
yuumuu <haidar.alhazen93@gmail.com>
2025-04-20 16:20:39 +0700

    Scripting Code on file struct_four.cpp

---
+++ struct_four.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+};
+
+int main(){
+    Mahasiswa mhs[2];
+
+    for(int i = 0; i < 2; i++) {
+        cout << "Mahasiswa ke-" << i+1 << endl;
+        cout << "Masukkan NIM           = ";
+        cin >> mhs[i].nim;
+        cin.ignore();
+        cout << "Masukkan Nama          = ";
+        getline(cin, mhs[i].nama);
+        cout << "Masukkan Alamat Desa   = ";
+        cin >> mhs[i].alamat.desa;
+        cout << "Masukkan Alamat Kota   = ";
+        cin >> mhs[i].alamat.kota;
+    }
+
+    cout << endl;
+
+    cout << "Data Mahasiswa" << endl;
+    
+    for (int i = 0; i < 2; i++) {
+    cout << "Data Mahasiswa ke-" << i+1 << endl;
+    cout << "NIM            = " << mhs[i].nim << endl;
+    cout << "Nama           = " << mhs[i].nama << endl;
+    cout << "Alamat Desa    = " << mhs[i].alamat.desa << endl;
+    cout << "Alamat Kota    = " << mhs[i].alamat.kota << endl;
+    }
+}
